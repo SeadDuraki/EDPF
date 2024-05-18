ac28805d280bed0f4a8924909120fd1a2894c98d
SeadDuraki <seadduraki12@gmail.com>
2024-05-18 19:05:00 +0200

    Update

--- js/slideshow/cbpBGSlideshow.js
+++ js/slideshow/cbpBGSlideshow.js
@@ -1,26 +1,16 @@
-/**
- * cbpBGSlideshow.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-var cbpBGSlideshow = (function() {
+var cbpBGSlideshow = (function () {
 
-	var $slideshow = $( '#cbp-bislideshow' ),
-		$items = $slideshow.children( 'li' ),
+	var $slideshow = $('#cbp-bislideshow'),
+		$items = $slideshow.children('li'),
 		itemsCount = $items.length,
-		$controls = $( '#cbp-bicontrols' ),
+		$controls = $('#cbp-bicontrols'),
 		navigation = {
-			$navPrev : $controls.find( 'span.cbp-biprev' ),
-			$navNext : $controls.find( 'span.cbp-binext' ),
-			$navPlayPause : $controls.find( 'span.cbp-bipause' )
+			$navPrev: $controls.find('span.cbp-biprev'),
+			$navNext: $controls.find('span.cbp-binext'),
+			$navPlayPause: $controls.find('span.cbp-bipause')
 		},
-		// current item´s index
-		current = 0,
+		// current item´s index (set to a random index)
+		current = Math.floor(Math.random() * itemsCount),
 		// timeout
 		slideshowtime,
 		// true if the slideshow is active
@@ -28,99 +18,99 @@ var cbpBGSlideshow = (function() {
 		// it takes 3.5 seconds to change the background image
 		interval = 3500;
 
-	function init( config ) {
+	function init(config) {
 
 		// preload the images
-		$slideshow.imagesLoaded( function() {
-			
-			if( Modernizr.backgroundsize ) {
-				$items.each( function() {
-					var $item = $( this );
-					$item.css( 'background-image', 'url(' + $item.find( 'img' ).attr( 'src' ) + ')' );
-				} );
+		$slideshow.imagesLoaded(function () {
+
+			if (Modernizr.backgroundsize) {
+				$items.each(function () {
+					var $item = $(this);
+					$item.css('background-image', 'url(' + $item.find('img').attr('src') + ')');
+				});
 			}
 			else {
-				$slideshow.find( 'img' ).show();
+				$slideshow.find('img').show();
 				// for older browsers add fallback here (image size and centering)
 			}
-			// show first item
-			$items.eq( current ).css( 'opacity', 1 );
+			// show the initial random item
+			$items.eq(current).css('opacity', 1);
 			// initialize/bind the events
 			initEvents();
 			// start the slideshow
 			startSlideshow();
 
-		} );
-		
+		});
+
 	}
 
 	function initEvents() {
 
-		navigation.$navPlayPause.on( 'click', function() {
+		navigation.$navPlayPause.on('click', function () {
 
-			var $control = $( this );
-			if( $control.hasClass( 'cbp-biplay' ) ) {
-				$control.removeClass( 'cbp-biplay' ).addClass( 'cbp-bipause' );
+			var $control = $(this);
+			if ($control.hasClass('cbp-biplay')) {
+				$control.removeClass('cbp-biplay').addClass('cbp-bipause');
 				startSlideshow();
 			}
 			else {
-				$control.removeClass( 'cbp-bipause' ).addClass( 'cbp-biplay' );
+				$control.removeClass('cbp-bipause').addClass('cbp-biplay');
 				stopSlideshow();
 			}
 
-		} );
+		});
 
-		navigation.$navPrev.on( 'click', function() { 
-			navigate( 'prev' ); 
-			if( isSlideshowActive ) { 
-				startSlideshow(); 
-			} 
-		} );
-		navigation.$navNext.on( 'click', function() { 
-			navigate( 'next' ); 
-			if( isSlideshowActive ) { 
-				startSlideshow(); 
+		navigation.$navPrev.on('click', function () {
+			navigate('prev');
+			if (isSlideshowActive) {
+				startSlideshow();
 			}
-		} );
+		});
+		navigation.$navNext.on('click', function () {
+			navigate('next');
+			if (isSlideshowActive) {
+				startSlideshow();
+			}
+		});
 
 	}
 
-	function navigate( direction ) {
+	function navigate(direction) {
 
 		// current item
-		var $oldItem = $items.eq( current );
-		
-		if( direction === 'next' ) {
+		var $oldItem = $items.eq(current);
+
+		if (direction === 'next') {
 			current = current < itemsCount - 1 ? ++current : 0;
 		}
-		else if( direction === 'prev' ) {
+		else if (direction === 'prev') {
 			current = current > 0 ? --current : itemsCount - 1;
 		}
 
 		// new item
-		var $newItem = $items.eq( current );
+		var $newItem = $items.eq(current);
 		// show / hide items
-		$oldItem.css( 'opacity', 0 );
-		$newItem.css( 'opacity', 1 );
+		$oldItem.css('opacity', 0);
+		$newItem.css('opacity', 1);
 
 	}
 
 	function startSlideshow() {
 
 		isSlideshowActive = true;
-		clearTimeout( slideshowtime );
-		slideshowtime = setTimeout( function() {
-			navigate( 'next' );
+		clearTimeout(slideshowtime);
+		slideshowtime = setTimeout(function () {
+			navigate('next');
 			startSlideshow();
-		}, interval );
+		}, interval);
 
 	}
 
 	function stopSlideshow() {
 		isSlideshowActive = false;
-		clearTimeout( slideshowtime );
+		clearTimeout(slideshowtime);
 	}
 
-	return { init : init };
+	return { init: init };
 
 })();
